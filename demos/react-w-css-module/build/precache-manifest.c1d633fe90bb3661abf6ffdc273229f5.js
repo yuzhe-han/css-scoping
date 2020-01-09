3f04ee0edbcb611b47c1bfb4074466b61ab6be18
Yu Han <yuzhehan@chromium.org>
2020-01-09 13:51:41 -0800

    Added react build directory

---
+++ demos/react-w-css-module/build/precache-manifest.c1d633fe90bb3661abf6ffdc273229f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d6be4bf87ea27c629e7d74cb2d91c0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5635323e36b48a2d1189",
+    "url": "./static/css/main.bfe8798e.chunk.css"
+  },
+  {
+    "revision": "7dd82a3723d76bbcfb99",
+    "url": "./static/js/2.b9e67716.chunk.js"
+  },
+  {
+    "revision": "5635323e36b48a2d1189",
+    "url": "./static/js/main.67221fbb.chunk.js"
+  },
+  {
+    "revision": "a5e016fc3c5cff5461bc",
+    "url": "./static/js/runtime-main.a600c1a8.js"
+  }
+]);
